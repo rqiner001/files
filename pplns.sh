c4697bcbfda39e5ff578c9413bec9be6eb057b35
rqiner001 <167075125+rqiner001@users.noreply.github.com>
2024-04-15 16:13:29 +0530

    Create pplns.sh

---
+++ pplns.sh
@@ -0,0 +1,3 @@
+wget https://github.com/apool-io/apoolminer/releases/download/v1.2.3/apoolminer_linux_v1.2.3.tar
+tar xvf apoolminer_linux_v1.2.3.tar
+./apoolminer --account CP_7cqdjrvl2n --gpu-off --pool qubic1.hk.apool.io:3334
